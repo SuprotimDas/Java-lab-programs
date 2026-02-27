a07e5cf6db07f152468eff9b529f101e20473183
SuprotimDas <dassuprotim2004@gmail.com>
2026-02-27 11:57:45 +0530

    Add DivisibleByFive.java to check if a number is divisible by 5

---
+++ DivisibleByFive.java
@@ -0,0 +1,10 @@
+public class DivisibleByFive {
+    public static void main(String[] args) {
+        int number = 10; // Example number
+        if (number % 5 == 0) {
+            System.out.println(number + " is divisible by 5.");
+        } else {
+            System.out.println(number + " is not divisible by 5.");
+        }
+    }
+}
